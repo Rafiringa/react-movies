63a654808e895ad6d87ddf9cf26b4f74cb0b1063
Ampy <rafiringa.a@gmail.com>
2023-11-06 16:11:35 +0300

    Debut du projet

---
+++ src/components/Navigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div className="header">
+      <ul>
+        <NavLink
+          to={"/"}
+          className={(nav) => (nav.isActive ? "nav-active hover" : "hover")}
+        >
+          <li>Accueil</li>{" "}
+        </NavLink>
+        <NavLink
+          to={"/favoris"}
+          className={(nav) => (nav.isActive ? "nav-active hover" : "hover")}
+        >
+          <li>Favoris</li>{" "}
+        </NavLink>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
